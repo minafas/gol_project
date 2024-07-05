2c37d3887869eb9508d0af7de918fc010e201f36
minafas <m.rahimizadeh49@gmail.com>
2024-07-05 11:47:50 +0330

    version 1_0_1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      "mobile":"0px",
+      "desktop":"1000px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
